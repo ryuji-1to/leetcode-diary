62240c00a4fe1fef6b511ee25fcc6df62014818a
ryuji-1to <ryuji0222ss@icloud.com>
2025-04-11 09:13:15 +0900

    🎉 4/11

--- src/problems/medium/p_0535.rs
+++ src/problems/medium/p_0535.rs
@@ -1,30 +1,48 @@
-use std::collections::HashMap;
-
 struct Codec {
-    dictionary: HashMap<String, String>,
+    dictionary: (String, String),
+    original: String,
+    scheme: String,
 }
 
 impl Codec {
     fn new() -> Self {
         Self {
-            dictionary: HashMap::new(),
+            dictionary: ("".to_string(), "".to_string()),
+            original: String::new(),
+            scheme: String::new(),
         }
     }
 
     fn encode(&mut self, longURL: String) -> String {
         let path = self.get_url_path(longURL);
-        todo!()
+        let split = path.split_once('/').unwrap();
+        let encoded = split
+            .1
+            .chars()
+            .collect::<Vec<_>>()
+            .chunks(2)
+            .map(|x| {
+                let a = x.get(0);
+                let b = x.get(1);
+                let a = if let Some(&c) = a { c as u8 } else { 1 };
+                let b = if let Some(&c) = b { c as u8 } else { 1 };
+                ((a + b) % 10 + b'a') as char
+            })
+            .collect::<String>();
+        let mut result = format!("{}://{}/{}", self.scheme, split.0, encoded);
+        self.original = split.0.to_string();
+        self.dictionary = (split.1.to_string(), encoded);
+        result
     }
 
     fn decode(&self, shortURL: String) -> String {
-        let url = self.get_url_path(shortURL);
-        todo!()
+        format!("{}://{}/{}", self.scheme, self.original, self.dictionary.0)
     }
 
-    fn get_url_path(&self, url: String) -> String {
-        let domain = url.split("://").collect::<Vec<_>>()[1];
-        let b = domain.split('/').nth(1).unwrap_or("");
-        b.to_string()
+    fn get_url_path(&mut self, url: String) -> String {
+        let domain = url.split("://").collect::<Vec<_>>();
+        self.scheme = domain[0].to_string();
+        domain[1].into()
     }
 }
 
@@ -34,13 +52,11 @@ mod test {
 
     #[test]
     fn it_works() {
-        // let url = "https://leetcode.com/problems/design-tinyurl";
-        // let expected = "https://leetcode.com/...";
-        //
-        // let c = Codec::new();
-        // let tiny = c.encode(url.to_string());
-        // assert_eq!(expected, tiny);
-        // let origin = c.decode(tiny.to_string());
-        // assert_eq!(url, origin);
+        let url = "https://leetcode.com/problems/design-tinyurl";
+
+        let mut c = Codec::new();
+        let encoded = c.encode(url.to_string());
+        let decoded = c.decode(encoded);
+        assert_eq!(url, decoded);
     }
 }
